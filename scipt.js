4af2296576b4473d86623efd02ab26a86bc66bdc
RedDx0 <141889141+RedDx0@users.noreply.github.com>
2025-03-04 19:23:51 +0100

    Create scipt.js

---
+++ scipt.js
@@ -0,0 +1,6 @@
+function downloadVersion(version) {
+    alert(`You clicked to download version ${version}.`);
+    // Here you can add the actual download logic or redirect to the download link
+    // For example:
+    // window.location.href = `downloads/minecraft-client-${version}.zip`;
+}
